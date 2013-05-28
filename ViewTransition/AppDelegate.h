8beacb1abbd3e017b0fcd0882bd11d4801125043
Kouichi ABE (WALL) <kouichi@magickworx.com>
2013-05-28 12:53:09 +0900

    Initial Commit

---
+++ ViewTransition/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewTransition
+//
+//  Created by Kouichi ABE (WALL) on 2013/05/28.
+//  Copyright (c) 2013年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewTransition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewTransition
+//
+//  Created by Kouichi ABE (WALL) on 2013/05/28.
+//  Copyright (c) 2013年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
